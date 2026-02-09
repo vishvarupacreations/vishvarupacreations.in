daa263e0b8a72205c6a0ce53cf1736193ee41374
vishvarupacreations <vishvarupacreationsdigitalstud@gmail.com>
2026-02-09 23:04:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function bookNow() {
+  alert("Thank you for choosing Vishvarupa Creations! Booking coming soon 🎬");
+}
+
+function becomeMember() {
+  alert("Membership program launching soon 🚀 Stay tuned!");
+}

0dbc35f858d95d5a58d7ff3046bf470f04ed6afa
vishvarupacreations <vishvarupacreationsdigitalstud@gmail.com>
2026-02-12 20:25:43 +0530

    Delete script.js
    delete message

--- script.js
+++
@@ -1,7 +0,0 @@
-function bookNow() {
-  alert("Thank you for choosing Vishvarupa Creations! Booking coming soon 🎬");
-}
-
-function becomeMember() {
-  alert("Membership program launching soon 🚀 Stay tuned!");
-}
